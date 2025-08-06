83e5b8251146351ca093bfd87d5f37745b08c3d9
Scholtz <ludko2@gmail.com>
2025-08-06 12:56:03 +0200

    signalr live

--- src/services/signalrService.ts
+++ src/services/signalrService.ts
@@ -4,16 +4,60 @@ import {
   LogLevel,
 } from "@microsoft/signalr";
 import type { AMMTrade } from "../types/algorand";
+import { generateAlgorandAccount } from "arc76";
+import { uuidv7 } from "uuidv7";
+import algosdk from "algosdk";
+import { Buffer } from "buffer";
+import { makeArc14AuthHeader, makeArc14TxWithSuggestedParams } from "arc14";
+import { SuggestedParams } from "algosdk";
 
 class SignalRService {
   private connection: HubConnection | null = null;
   private isConnected = false;
   private reconnectInterval: number | null = null;
-
+  async getAuthToken(): Promise<string> {
+    let session = "";
+    try {
+      const sessionLS = localStorage.getItem("session");
+      if (sessionLS) session = sessionLS;
+    } catch {
+      console.log("session not found");
+    }
+    if (!session) {
+      session = uuidv7();
+      localStorage.setItem("session", session);
+    }
+    // Implement your logic to retrieve the authentication token
+    const account: algosdk.Account = await generateAlgorandAccount(session);
+    const params: SuggestedParams = {
+      fee: 1000n,
+      genesisHash: new Uint8Array(
+        Buffer.from("wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=", "base64")
+      ),
+      genesisID: "mainnet-v1.0",
+      lastValid: 46916880n,
+      minFee: 1000n,
+      flatFee: false,
+      firstValid: 46915880n,
+    };
+    const tx = await makeArc14TxWithSuggestedParams(
+      "BiatecScan#ARC14",
+      account.addr.toString(),
+      params
+    );
+    const signed = tx.signTxn(account.sk);
+    const header = makeArc14AuthHeader(signed);
+    return header;
+  }
   async connect(): Promise<void> {
     try {
+      const headers = {
+        authorization: await this.getAuthToken(),
+      };
       this.connection = new HubConnectionBuilder()
-        .withUrl("wss://api.scan.biatec.io/tradehub") // Biatec scan API SignalR endpoint
+        .withUrl("https://algorand-trades.de-4.biatec.io/biatecScanHub", {
+          headers: headers,
+        }) // Biatec scan API SignalR endpoint
         .withAutomaticReconnect()
         .configureLogging(LogLevel.Information)
         .build();
